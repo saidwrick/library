f5b3d516a3f9068437db73a5c0b298da1ea8792e
saidwrick <cedric.a.fung@gmail.com>
2022-03-10 04:41:24 -0500

    finished base app

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-let myLibrary = [];
-
-function Book(title, author, pages, read){
+function randomColor(){
+    return "#"+ Math.floor(Math.random()*16777215).toString(16);
+}
+function Book(title, author, pages, read, color){
     this.title = title
     this.author = author
     this.pages = pages 
     this.read = read
+    this.color = color
 }
 
 function addBookToLibrary(){
-
+    const newBook = new Book(formTitle.value, formAuthor.value, formPages.value, formRead.checked, randomColor());
+    myLibrary.push(newBook);
 }
 
-function displayBook(book){
+function displayBook(book, index){
     let card = document.createElement('div');
     card.classList.add("card");
-    card.setAttribute('data', '0');
+    card.setAttribute('data', index);
     
     let title = document.createElement('div');
     title.classList.add("title");
     title.innerHTML = book.title;
+    title.style.backgroundColor=book.color;
 
     let del = document.createElement('div');
     del.classList.add("delete");
@@ -55,29 +59,86 @@ function displayBook(book){
     library.appendChild(card);
 }
 
+function displayAllBooks(array){
+    library.innerHTML="";
+    array.forEach((book, index)=>{
+        displayBook(book, index);
+    })
+}
+
+function formReset(){
+    formPrompt.style.visibility = "hidden";
+    formTitle.value = "";
+    formAuthor.value = "";
+    formPages.value = "";
+    formRead.checked = false;
+}
+
+let myLibrary = [];
+
 const library = document.querySelector(".library");
+
+// card delete button
 
 document.body.addEventListener('click', function(e){
     if (e.target.className == 'delete'){
-        e.target.parentNode.parentNode.remove();
+        myLibrary.splice((e.target.parentNode.parentNode.getAttribute('data')), 1);
+        displayAllBooks(myLibrary);
     }
 });
+
+// read/unread button
 
 document.body.addEventListener('click', function(e){
     if (e.target.className == "read"){
             e.target.className = "unread";
             e.target.innerHTML = "unread";
+            myLibrary[e.target.parentNode.parentNode.getAttribute('data')].read = false;
     }
     else if (e.target.className == "unread"){
         e.target.className = "read";
         e.target.innerHTML = "read";
-        console.log(e.target.parentNode.parentNode.getAttribute('data'));
+        myLibrary[e.target.parentNode.parentNode.getAttribute('data')].read = true;
     }
 });
 
-const test = new Book("Big Book", "Jimmy", "205", false);
 
-console.table(test);
+// form 
+const formPrompt = document.querySelector(".prompt");
+const formTitle = document.querySelector("#title");
+const formAuthor = document.querySelector("#author");
+const formPages = document.querySelector("#pages");
+const formRead = document.querySelector("#read");
+
+const formButton = document.querySelector("#form-submit");
+formButton.addEventListener('click', function(e){
+    e.preventDefault();
+    if(formTitle.value != "" && formAuthor.value !="" && formPages.value !=""){
+        addBookToLibrary();
+        formReset();
+        displayAllBooks(myLibrary);
+    }
+    else{
+        null;
+    }
+});
+
+const formClose = document.querySelector(".close-button");
+formClose.addEventListener('click', function(){
+    formReset();
+});
+
+const addBookButton = document.querySelector(".add-book-button");
+addBookButton.addEventListener('click', function(){
+    formPrompt.style.visibility = "visible";
+});
 
 
-displayBook(test);
+
+const filler1 = new Book("The Big Book", "Jimmy Neutron", "205", false, randomColor());
+const filler2 = new Book("Really Long Book Title Name Forever", "James Cameron", "404", true, randomColor());
+const filler3 = new Book("Best Book Ever", "James Bond", 1000, false, randomColor());
+
+myLibrary.push(filler1, filler2, filler3);
+
+displayAllBooks(myLibrary);
